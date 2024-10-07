b1e068f10bb9be4e30d94a10a0022c88003cd2b1
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-10-08 04:32:55 +0530

    feat: updated configuration

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,12 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    ts = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascriptreact = { "prettier" },
   },
 
   -- format_on_save = {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,45 @@ return {
     end,
   },
 
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "prettier",
+      },
+    },
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {
+      -- Defaults
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false, -- Auto close on trailing </
+    },
+    -- Also override individual filetype configs, these take priority.
+    lazy = false,
+    -- Empty by default, useful if one of the "opts" global settings
+    -- doesn't work well in a specific filetype
+  },
+  {
+    "APZelos/blamer.nvim",
+    lazy = false,
+  },
+  { "wakatime/vim-wakatime", lazy = false },
+  {
+    "supermaven-inc/supermaven-nvim",
+    lazy = false,
+    config = function()
+      require("supermaven-nvim").setup {}
+    end,
+  },
+  {
+    "HiPhish/nvim-ts-rainbow2",
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
